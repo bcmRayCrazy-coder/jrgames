0fe140a5a86b11d566dbb5f882cbb10ca75dc9cb
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2023-01-16 09:05:05 +0800

    [change] 修改基类Game: 将监听器扩展为多个监听器; [fix] 修复了无法setGameName的问题

--- src/main/java/cn/jerrymc/jrgames/games/Game.java
+++ src/main/java/cn/jerrymc/jrgames/games/Game.java
@@ -1,7 +1,12 @@
 package cn.jerrymc.jrgames.games;
 
 import cn.jerrymc.jrgames.Jrgames;
+import cn.jerrymc.jrgames.games.events.PlayerJoinGameEvent;
+import cn.jerrymc.jrgames.games.events.PlayerLeaveGameEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
 
@@ -9,7 +14,9 @@ public class Game {
     private final Jrgames plugin;
     private final Number tick = 0;
     private String gameName = "";
-    private GameState gameState;
+    private String displayName = "";
+    private GameState gameState = GameState.WAITING;
+    private ArrayList<Listener> gameListeners;
 
     public ArrayList<Player> players;
 
@@ -17,20 +24,36 @@ public class Game {
         this.plugin = plugin;
     }
 
+    public void gameStopHandler(){
+        // 停止监听
+        for(Listener l : getGameListeners()){
+            HandlerList.unregisterAll(l);
+        }
+    }
+
     public void onTick(Number tick){}
 
     public void onPlayerJoin(Player p){
         players.add(p);
+        Bukkit.getPluginManager().callEvent(new PlayerJoinGameEvent(p,getGameName()));
     }
     public void onPlayerLeave(Player p){
         players.remove(p);
+        Bukkit.getPluginManager().callEvent(new PlayerLeaveGameEvent(p,getGameName()));
     }
 
     public void setGameName(String name){
-        this.gameName = gameName;
+        this.gameName = name;
     }
     public String getGameName() {
         return gameName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    public String getDisplayName() {
+        return displayName;
     }
 
     public void setGameState(GameState gameState) {
@@ -39,6 +62,15 @@ public class Game {
     public GameState getGameState() {
         return gameState;
     }
+
+    public void registerGameListener(Listener listener){
+        getGameListeners().add(listener);
+        getPlugin().getServer().getPluginManager().registerEvents(listener,getPlugin());
+    }
+    public ArrayList<Listener> getGameListeners() {
+        return gameListeners;
+    }
+
     public Jrgames getPlugin() {
         return plugin;
     }
